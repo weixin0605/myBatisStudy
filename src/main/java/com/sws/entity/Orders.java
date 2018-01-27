0dac58bddc1244e247eaac1d1a9656b767e9c5d0
weixin0605 <weixin0605@163.com>
2018-01-27 17:11:30 +0800

    mybatis study

---
+++ src/main/java/com/sws/entity/Orders.java
@@ -0,0 +1,42 @@
+package com.sws.entity;
+
+import java.util.Date;
+
+public class Orders {
+    private Integer id;
+    private Integer userId;
+    private Date createtime;
+    private String note;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
