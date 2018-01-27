fccad156f834b9b39a199abc6128ef9fc98c3403
weixin0605 <weixin0605@163.com>
2018-01-27 17:11:12 +0800

    mybatis study

---
+++ src/main/java/com/sws/entity/OrderCustom.java
@@ -0,0 +1,37 @@
+package com.sws.entity;
+
+//order订单扩展类
+public class OrderCustom extends Orders{
+
+
+    //添加用户信息
+    private  String username;
+    private String sex;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
