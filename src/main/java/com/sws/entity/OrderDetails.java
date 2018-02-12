fef43ddd85500f0e552356c1b0ff592ff41756fc
weixin0605 <weixin0605@163.com>
2018-02-12 17:43:18 +0800

    修改

---
+++ src/main/java/com/sws/entity/OrderDetails.java
@@ -0,0 +1,49 @@
+package com.sws.entity;
+
+public class OrderDetails {
+    private String id;
+    private String ordersId;
+    private String itemId;
+    private String itemName;
+    private Items items;
+
+    public Items getItems() {
+        return items;
+    }
+
+    public void setItems(Items items) {
+        this.items = items;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrdersId() {
+        return ordersId;
+    }
+
+    public void setOrdersId(String ordersId) {
+        this.ordersId = ordersId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
--- src/main/java/com/sws/mapper/OrderMapper.java
+++ src/main/java/com/sws/mapper/OrderMapper.java
@@ -1,4 +1,11 @@
 package com.sws.mapper;
 
+import com.sws.entity.Orders;
+
+import java.util.List;
+
 public interface OrderMapper {
+    public List<Orders> findOrdersUserResultMap()throws Exception;
+
+    public List<Orders> findOrdersUserAndOrderDetailResultMap() throws Exception;
 }
